1737f00de5c7866b1b071c6fa3d09d6da2dc91a1
Neil <neil@premiergsolutions.com>
2021-04-25 08:11:18 +0200

    Changed rotation to be relative to the world.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -15,21 +15,32 @@ public class Controller : MonoBehaviour
     public InputActionMap actionMap;
     public InputAction rotateCounterClockwise;
     public InputAction rotateClockwise;
+    public InputAction rotateUp;
+    public InputAction rotateDown;
+
 
     public bool RotatingCounterClockwise;
     public bool RotatingClockwise;
+    public bool RotatingUp;
+    public bool RotatingDown;
+
+
+
     public bool RotatingClockwiseSecondMethod;
 
-    private float rotateSpeed;
     [SerializeField]
     public float RotateSpeedControl;
 
+    private float rotateClockwiseSpeed;
+    private float rotateUpSpeed;
+  
+    
     public int count;
 
     // Start is called before the first frame update
     void Start()
     {
-        rotateSpeed = 0;
+        rotateClockwiseSpeed = 0;
     }
 
     private void Awake()
@@ -37,6 +48,10 @@ public class Controller : MonoBehaviour
         actionMap = playerInput.actions.FindActionMap("Player");
         rotateCounterClockwise = actionMap.FindAction("RotateCounterClockwise");
         rotateClockwise = actionMap.FindAction("RotateClockwise");
+        rotateUp = actionMap.FindAction("RotateUp");
+        rotateDown = actionMap.FindAction("RotateDown");
+
+
 
         //Example, calling delegate without specifying any parameters
         rotateClockwise.performed += OnRotateClockwise;
@@ -44,13 +59,16 @@ public class Controller : MonoBehaviour
         //Example, calling delegate while specifying the full parameters. This is identical to the example above
         //ctx stands for "context" and seems to be a standish way of naming the parameter
         rotateCounterClockwise.performed += ctx => OnRotateCounterClockwise(ctx);
-
         //Example, doing a bit of logic on the parameter and then calling the delegate
         rotateClockwise.performed += ctx => OnRotateClockwiseWithBoolean (ctx.ReadValue<float>() > 0);
 
         //Example, just doing some logic but not actually calling a delegate
         rotateClockwise.performed += _ => count++;
+     
+        rotateUp.performed += ctx => OnRotateUp(ctx);
+        rotateDown.performed += ctx => OnRotateDown(ctx);
     }
+
 
     //Note, if the delegate is called the same as the action in the action map 
     //then Unity will try and invoke it automatically if the Player Input behaviour is set to Send Messages
@@ -74,23 +92,48 @@ public class Controller : MonoBehaviour
         RotatingCounterClockwise = obj.ReadValue<float>() > 0;
     }
 
+    private void OnRotateUp(InputAction.CallbackContext obj)
+    {
+        RotatingUp = obj.ReadValue<float>() > 0;
+    }
+
+
+    private void OnRotateDown(InputAction.CallbackContext obj)
+    {
+        RotatingDown = obj.ReadValue<float>() > 0;
+    }
+
 
 
     void FixedUpdate()
     {
         if (RotatingClockwise)
         {
-            rotateSpeed -= RotateSpeedControl;
+            rotateClockwiseSpeed -= RotateSpeedControl;
         }
 
         if (RotatingCounterClockwise)
         {
-            rotateSpeed += RotateSpeedControl;
+            rotateClockwiseSpeed += RotateSpeedControl;
         }
 
-        world.transform.Rotate(0, 0, rotateSpeed);
+        if (RotatingUp)
+        {
+            rotateUpSpeed += RotateSpeedControl;
+        }
+
+        if (RotatingDown)
+        {
+            rotateUpSpeed -= RotateSpeedControl;
+        }
 
 
+        var rotation = new Vector3(rotateUpSpeed, 0, rotateClockwiseSpeed);
+        
+        //Rotate the world gameobject relative to the whole world
+        //This is done so that when we try and control the object the motions are
+        //sensibly relative to the world. rather than the current orientation of the object
+        world.transform.Rotate(rotation,Space.World);
 
     }
 }
